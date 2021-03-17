d0982b1acd45602da06c2042d7bad05fbd9766c9
Gerrod Wagner <gerrodwagner@gmail.com>
2021-03-17 17:00:58 -0400

    script move

---
+++ Script/app.js
@@ -0,0 +1,18 @@
+/* Animation */
+
+/*
+function myMove() {
+    var elem = document.getElementById("warning")
+    var pos = 0;
+    var id = setInterval(frame,10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id)
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+*/
