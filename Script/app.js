ae803c269279843585907d3d89c645bc58785ee1
Gerrod Wagner <gerrodwagner@gmail.com>
2021-03-30 12:44:20 -0400

    new -GCW

--- Script/app.js
+++ Script/app.js
@@ -1,27 +1,6 @@
-/* Animation */
 
-/*
-function myMove() {
-    var elem = document.getElementById("warning")
-    var pos = 0;
-    var id = setInterval(frame,10);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id)
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
+
+  function myFunction() {
+  alert("Hello! I am Gerrod I am still learning Javascript so this is my best give me some time, will have done soon thanks!");
     }
-}
-*/
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+ 
