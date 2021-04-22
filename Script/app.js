c4860fce82076de535274d9fcaee117d419b807a
Gerrod Wagner <gerrodwagner@gmail.com>
2021-04-22 10:13:55 -0400

    final

--- Script/app.js
+++ Script/app.js
@@ -4,3 +4,28 @@
   alert("Hello! I am Gerrod I am still learning Javascript so this is my best give me some time, will have done soon thanks!");
     }
  
+
+
+
+
+
+
+
+    const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
